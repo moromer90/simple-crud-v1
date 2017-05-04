6fa4d64a8e5a8be97c5e4822848c9432330c914a
jose <jdavid.conesa@ikasle.aeg.es>
2017-05-04 15:48:40 +0200

    modificado routers y userController para hacer un getAll

--- routers.js
+++ routers.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const mongoose =  require ('mongoose');
 const app = express();
-const controller = require("controllers/userController");
+const controller = require("./controllers/userController");
 //const validation = require("./middleware");
 
 
 app.route("/")
-    .get(controller.getAll)
-    .post(/*validation,*/controller.post)
+    .get(controller.getUser)
+//    .post(/*validation,*/controller.post)
          
 app.route("/:_id")
-    .get(controller.getById)
-    .put(/*validation,*/controller.put)
-    .delete(controller.delete)
+//    .get(controller.getById)
+//    .put(/*validation,*/controller.put)
+//    .delete(controller.delete)
 
 
 
