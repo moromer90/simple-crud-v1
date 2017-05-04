aeaad7d9edcdb6db4cf7517535d494a6f30d5b15
jose <jdavid.conesa@ikasle.aeg.es>
2017-05-04 15:16:27 +0200

    añadido fichero routers.js

---
+++ routers.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose =  require ('mongoose');
+const app = express();
+const controller = require("controllers/userController");
+//const validation = require("./middleware");
+
+
+app.route("/")
+    .get(controller.getAll)
+    .post(/*validation,*/controller.post)
+         
+app.route("/:_id")
+    .get(controller.getById)
+    .put(/*validation,*/controller.put)
+    .delete(controller.delete)
+
+
+
+module.exports = app;
+
+
