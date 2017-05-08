c7f26af459a63b70eee8a96476eaa1e0a48fbf98
Alex <aliaksei@ikasle.aeg.es>
2017-05-08 14:43:39 +0200

    no funcciona POST

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,20 +12,45 @@ exports.getUser = function (req,res) {
 
 
 exports.postUser = function (req,res) {
-	console.log(req.body)
+	console.log("req.body = "+req.body)
     var newUser = new User({   name: req.body.name,
                                email:req.body.email,
                                pass: req.body.pass,
                                active:req.body.active,
                                img: req.body.img
                              });
-     console.log(newUser);
+     console.log("NewUser = "+newUser);
             newUser.save(function(err,user){
                 if(err){
                     return res.send("Error: "+ err.message + ".....");
                 }
-                console.log(newUser);
+                console.log("NewUser function SAVE true = "+newUser);
                 res.send(newUser + "\n Insert realizado con exito");
             });
->>>>>>> 4d4b47ee350c7c3f839c94d36d527953dca89c5e
+
+}
+
+exports.updateUser = function (req,res) {
+    var userId = req.params._id;
+    var update = req.body;
+    console.log(userId);
+    console.log(update);
+    User.findOneAndUpdate(userId,update,{new:true},function(err,user){
+        if(err){
+            return res.send("Error: "+ err.message);
+        }
+        console.log(user);
+        res.send(user + "\n Actualizacion realizada con exito");
+    });
+}
+
+exports.deleteUser = function (res,req){
+    User.findOneAndRemove({_id:req.params._id},function(err,user){
+        if(err){
+            return res.send("Error: "+ err.message);
+        }
+        console.log(user);
+        res.send(user + "\n Borrado realizado con exito");
+    });
+
 }
