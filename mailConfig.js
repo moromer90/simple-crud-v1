b16d3887b83e463797fbabe772fee82c2df970ec
jose <jdavid.conesa@ikasle.aeg.es>
2017-05-16 20:31:21 +0200

    mailconfigcreado

---
+++ mailConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mailUser :"jdavid.conesa@ikasle.aeg.es",
+    clientId:"203752607426-u342pc85lrj8mse6mmrtjmdpdokpv9sj.apps.googleusercontent.com",
+    clientSecret:"x-v5nHVZywn57yfpsOD5_ksC",
+	refreshToken: "1/jTw60xtbVh1t88ZEwH3b0KmTyZKBGZ3vWL6PGEFKXSk"
+}
+    
