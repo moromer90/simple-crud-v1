705d1c016dbfc86125d35652cfa31d436877485d
jose <jdavid.conesa@ikasle.aeg.es>
2017-05-16 20:30:34 +0200

    probando crear gmail api desde el princio

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -2,28 +2,28 @@ var nodemailer = require("nodemailer");
 const xoauth2 = require('xoauth2');
 
 
-exports.emailSender= function (req,res) {
-    console.log("emailSender");
-    
-    var smtpTransport = nodemailer.createTransport({
+var smtpTransport = nodemailer.createTransport({
     service: "gmail",
     auth: {
         
         xoauth2: xoauth2.createXOAuth2Generator({
-                user: "jdavid.conesa@ikasle.aeg.es",
-                clientId:"203752607426-u342pc85lrj8mse6mmrtjmdpdokpv9sj.apps.googleusercontent.com",
-                clientSecret:"x-v5nHVZywn57yfpsOD5_ksC",
-                accessToken: "ya29.GltMBEy_hF1CDg2UALfCAwHhE_lHy7BfO2E1dEut-86WaEdALspYsRd0CNRsHBNyF-EdsIXCUdfIocApLd1suSKBRL5i50-cZXadfC_4kK-u6C-ZFs1GHP5iqwTq", 
-                refreshToken: "1/NX91F0URfGxf795INtYx7J_abtXB9CnQfDpiMWH3zfQeohxy5FrD8GBWsnzYH25f"
+                user:"jdavid.conesa@ikasle.aeg.es",
+                clientId:"953592784627-pekpf99f62lnkpjc5nkbnrvdq1260h80.apps.googleusercontent.com",
+                clientSecret:"UTOTU5ITvsvyVpBxa9VQDuZp",
+                refreshToken:"1/CPJUoIuyWkJdOtDouctJwZGyjsB-w-C76IaBgD6Fv40"
             }
         )
-        
-    }
-        
+    }     
 });
+
+exports.emailSender= function (req,res) {
+    console.log("emailSender");
+    
+    
     console.log(req.body.to);
 //    console.log("xoauth: " +JSON.stringify(auth));
     var mailOptions={
+        from:"jdavid.conesa@ikasle.aeg.es",
         to : req.body.to,
         subject : req.body.subject,
         text : req.body.text
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require ('../models/userModel')
+const User = require ('../models/userModel');
 
 exports.getUser = function (req,res) {
     User.find({}, function(err,user){
