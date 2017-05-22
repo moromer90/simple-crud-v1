eb89d6bcfaae3560692a8e384eea2b052621cd00
Alex <aliaksei@ikasle.aeg.es>
2017-05-22 20:16:43 +0200

    Validacion de unico email esta hecho bien con medio de mongoDb Se queda implementar validator y comprobar formulario

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 //Define our Mongoose Schema. Defined structure for all projects
 var userSchema = new mongoose.Schema({
     name:  String,
-    email: {type: String,  required: [true, 'Email line can\'t be empty'], index: { unique: true }, lowercase: true },
+    // email: {type: String,  required: [true, 'Email line can\'t be empty'], index: { unique: true }, lowercase: true },
+	email: {type: String, trim: true, index: true, unique: true},
 	pass: String,
     active: {type: String, default: 'false'}
 });
