5c060f5938ed646eeedba5baa364cef6d59d4042
jose <jdavid.conesa@ikasle.aeg.es>
2017-05-22 16:30:34 +0200

    orden de activacion cambiado

--- controllers/activeAccountController.js
+++ controllers/activeAccountController.js
@@ -1,7 +1,25 @@
+const User = require ('../models/userModel');
+const updateActive = require ('../controllers/userController');
+
 exports.getActiveAccountView = function(req,res){
+    
+    console.log("updateUserById");
+    var userId = req.params._id;
+    var update = {active:"true"};
+    
+    //    console.log(userId);
+    console.log(update);
+    User.findOneAndUpdate(userId,{$set:update},{new:true},function(err,user){
+        if(err){
+            return res.send("Error: "+ err.message);
+        }
+        console.log(user);
+        res.send(user);
+    });
+    
     res.render('../views/activeAccount.hbs',{
     pageTitle:"ActiveAccount",
-//    content:"Contenido de la página"
+    content:"Usuario activado"
     });
         
 }
