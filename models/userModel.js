a79c6f778e1b64f274f0c8192d621847b3697b85
Alex <aliaksei@ikasle.aeg.es>
2017-05-23 13:40:42 +0200

    started validation but then checked out

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
     name:  String,
     // email: {type: String,  required: [true, 'Email line can\'t be empty'], index: { unique: true }, lowercase: true },
-	email: {type: String, trim: true, index: true, unique: true},
+	email: {type: String, trim: true, required: [true, 'Email line can\'t be empty'], index: true, unique: true},
 	pass: String,
     active: {type: String, default: 'false'}
 });
