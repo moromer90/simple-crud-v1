b6d20db3f69ddac037d293cea7363ed9335b592d
Alex <aliaksei@ikasle.aeg.es>
2017-05-27 12:07:12 +0200

    logoutadmin session control

--- public/js/sessionControl.js
+++ public/js/sessionControl.js
@@ -3,3 +3,9 @@ var data = sessionStorage.getItem('session');
 if (!data){
 	location.href = baseUrl;
 }
+
+$("#logout").click(function() {
+	ses_id="";
+	sessionStorage.setItem('session', ses_id);
+    location.href = baseUrl;
+}
